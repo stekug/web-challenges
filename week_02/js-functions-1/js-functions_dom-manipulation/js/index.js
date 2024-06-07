99d4264b6f9226ad1ed02fbbd242a9d1c0148b46
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-06-07 11:20:59 +0200

    js-functions_dom-manipulation

---
+++ week_02/js-functions-1/js-functions_dom-manipulation/js/index.js
@@ -0,0 +1,54 @@
+const box = document.querySelector("[data-js=box]");
+const redButton = document.querySelector("[data-js=js-red-button]");
+const greenButton = document.querySelector("[data-js=js-green-button]");
+const blueButton = document.querySelector("[data-js=js-blue-button]");
+const grayButton = document.querySelector("[data-js=js-gray-button]");
+
+/*
+We want to add some functionality to the buttons on this page. When pressing a button, the corresponding color should be applied to the box.
+  1: Create a function removeAllColors which removes all color classes from the box.
+  2: Create a function addColor which takes a class name as a parameter and adds the class to the box.
+  3: Use the two functions in the event listeners of the buttons to color in the box when the button is clicked.
+*/
+
+redButton.addEventListener("click", () => {
+  removeColor(redButton);
+  addColor(redButton, "red");
+  // use your functions to
+  // - remove all classes
+  // - then add the class name: "red"
+});
+
+blueButton.addEventListener("click", () => {
+  removeColor(blueButton);
+  addColor(blueButton, "blue");
+  // use your functions to
+  // - remove all classes
+  // - then add the class name: "blue"
+});
+
+greenButton.addEventListener("click", () => {
+  removeColor(greenButton);
+  addColor(greenButton, "green");
+  // use your functions to
+  // - remove all classes
+  // - then add the class name: "green"
+});
+
+grayButton.addEventListener("click", () => {
+  removeColor(grayButton);
+  addColor(grayButton, "gray");
+  // use your functions to
+  // - remove all classes
+  // - then add the class name: "gray"
+});
+
+// Write your two functions below:
+
+function addColor(button, color) {
+  button.classList.add(color);
+}
+
+function removeColor(button) {
+  button.className = "";
+}
