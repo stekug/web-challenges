14c1d0a95ab76b67938fd3298d8a2677b6af8432
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-06-17 15:56:52 +0200

    add solution for js-fetch_color-clue

---
+++ week_04/js-fetch/js-fetch_color-clue/js/fetch.js
@@ -0,0 +1,31 @@
+import { setColorToGuess, getRandomHexCode } from "./utils.js";
+
+export async function fetchNewColor() {
+  const hexCode = getRandomHexCode();
+  const colorApiUrl = `https://www.thecolorapi.com/id?hex=${hexCode}`;
+  const res = await fetch(colorApiUrl);
+  // console.log(res);
+  const data = await res.json();
+  // console.log(data);
+  const hex = data.name.closest_named_hex;
+  const value = data.name.value;
+  setColorToGuess(hex, value);
+
+  /**
+   * Hint 1:
+   * Use the fetch API to get the hex value and the name of the closest
+   * named color to the randomly generated `hexCode` from the color API.
+   * These values can be found in `.name.closest_named_hex` and
+   * `.name.value` properties of the response data respectively.
+   *
+   * Hint 2:
+   * Call the `setColorToGuess` function to set the color to guess.
+   * The function takes two arguments:
+   *  - the hex code of the closest named color
+   *  - and the name of the random color
+   */
+
+  // --v-- your code here --v--
+
+  // --^-- your code here --^--
+}
