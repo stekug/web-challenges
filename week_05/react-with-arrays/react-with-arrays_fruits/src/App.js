ffc40401ce64e1df9aad978d09603237946985bc
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-06-25 11:16:16 +0200

    add solution for react-with-arrays_fruits Update

--- week_05/react-with-arrays/react-with-arrays_fruits/src/App.js
+++ week_05/react-with-arrays/react-with-arrays_fruits/src/App.js
@@ -2,16 +2,16 @@ import Card from "./components/Card";
 
 export default function App() {
   const fruits = [
-    { id: 1, name: "🍎 Apple", color: "red" },
-    { id: 2, name: "🍌 Banana", color: "yellow" },
-    { id: 3, name: "🍇 Grape", color: "purple" },
-    { id: 4, name: "🍊 Orange", color: "orange" },
-    { id: 5, name: "🍋 Lemon", color: "yellow" },
-    { id: 6, name: "🥝 Kiwi", color: "green" },
-    { id: 7, name: "🫐 Blueberry", color: "blue" },
-    { id: 8, name: "🍓 Strawberry", color: "red" },
-    { id: 9, name: "🍍 Pineapple", color: "brown" },
-    { id: 10, name: "🍉 Watermelon", color: "green" },
+    { id: "1", name: "🍎 Apple", color: "red" },
+    { id: "2", name: "🍌 Banana", color: "yellow" },
+    { id: "3", name: "🍇 Grape", color: "purple" },
+    { id: "4", name: "🍊 Orange", color: "orange" },
+    { id: "5", name: "🍋 Lemon", color: "yellow" },
+    { id: "6", name: "🥝 Kiwi", color: "green" },
+    { id: "7", name: "🫐 Blueberry", color: "blue" },
+    { id: "8", name: "🍓 Strawberry", color: "red" },
+    { id: "9", name: "🍍 Pineapple", color: "brown" },
+    { id: "10", name: "🍉 Watermelon", color: "green" },
   ];
 
   return (
