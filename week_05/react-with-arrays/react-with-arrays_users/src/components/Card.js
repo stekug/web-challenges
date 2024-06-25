be1a5b0588f8f5a25c0bce79cea61c96e605d0a1
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-06-25 12:24:43 +0200

    add solution for react-with-arrays_users

---
+++ week_05/react-with-arrays/react-with-arrays_users/src/components/Card.js
@@ -0,0 +1,22 @@
+import "./Card.css";
+import Tag from "./Tag";
+import Button from "./Button";
+function Card({ user }) {
+  const { name, roles, about } = user;
+
+  return (
+    <article className="card">
+      <h2>{name}</h2>
+      <ul className="card__taglist">
+        {roles.map((role) => (
+          <Tag key={role} tag={role} />
+        ))}
+      </ul>
+      <p>{about}</p>
+      <Button>edit</Button>
+      <Button buttonType="danger">invoice</Button>
+    </article>
+  );
+}
+
+export default Card;
